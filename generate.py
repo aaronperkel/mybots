0f894af6c7ac19ce87eb9ba6dc407859e52c98ac
aaronperkel <aaron.perkel27@gmail.com>
2025-01-23 21:14:54 -0500

    grid of tapered towers

--- generate.py
+++ generate.py
@@ -6,9 +6,9 @@ start = 1
 sizes = [start * (0.9 ** i) for i in range(10)]
 
 for i in range(10):
-    x = 0
-    y = 0
-    z = sizes[i] / 2
-    pyrosim.Send_Cube(name=f"Box{i}", pos=[x,y,z+i] , size=[sizes[i],sizes[i],sizes[i]])
+    for x in range(-2, 2):
+        for y in range(-2, 2):
+            z = sizes[i] / 2
+            pyrosim.Send_Cube(name=f"Box{i}", pos=[x,y,z+i] , size=[sizes[i],sizes[i],sizes[i]])
 
 pyrosim.End()
