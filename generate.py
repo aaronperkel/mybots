371f331bb5bf336db9d62fc5bb08028ec16f9dfa
aaronperkel <aaron.perkel27@gmail.com>
2025-01-23 21:05:17 -0500

    tower

--- generate.py
+++ generate.py
@@ -10,7 +10,7 @@ x = 0
 y = 0
 z = height / 2
 
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-pyrosim.Send_Cube(name="Box2", pos=[x+1,y,z+1] , size=[length,width,height])
+for i in range(10):
+    pyrosim.Send_Cube(name=f"Box{i}", pos=[x,y,z+i] , size=[length,width,height])
 
 pyrosim.End()
