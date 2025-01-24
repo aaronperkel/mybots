b6fa59f8faf21b3c29df5515511c4f80b273bde8
aaronperkel <aaron.perkel27@gmail.com>
2025-01-23 20:56:19 -0500

    rectangle!

--- simulate.py
+++ simulate.py
@@ -3,6 +3,7 @@ import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0,0,-9.8)
