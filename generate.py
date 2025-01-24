b5bea927420dc039f72350a313441ebbcd919272
aaronperkel <aaron.perkel27@gmail.com>
2025-01-23 21:11:05 -0500

    decreasing tower

--- generate.py
+++ generate.py
@@ -2,15 +2,13 @@ import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("boxes.sdf")
 
-length = 1
-width = 1
-height = 1
-
-x = 0
-y = 0
-z = height / 2
+start = 1
+sizes = [start * (0.9 ** i) for i in range(10)]
 
 for i in range(10):
-    pyrosim.Send_Cube(name=f"Box{i}", pos=[x,y,z+i] , size=[length,width,height])
+    x = 0
+    y = 0
+    z = sizes[i] / 2
+    pyrosim.Send_Cube(name=f"Box{i}", pos=[x,y,z+i] , size=[sizes[i],sizes[i],sizes[i]])
 
 pyrosim.End()
