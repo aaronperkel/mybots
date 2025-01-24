f8ae9ce4ccf3e49d6d1753bf5ca8b7b46b89ea2c
aaronperkel <aaron.perkel27@gmail.com>
2025-01-24 12:49:21 -0500

    edited timing

--- simulate.py
+++ simulate.py
@@ -12,9 +12,13 @@ planeId = p.loadURDF("plane.urdf")
 
 p.loadSDF("boxes.sdf")
 
-for i in range(2000):
+# for i in range(2000):
+#     p.stepSimulation()
+#     time.sleep(1/60)
+#     print(i)
+
+while True:
     p.stepSimulation()
-    time.sleep(1/60)
-    print(i)
+    time.sleep(.005)
 
 p.disconnect()
