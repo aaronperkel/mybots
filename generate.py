2b2d389ba5ae11597d56093d65e9f9c84ee4c227
aaronperkel <aaron.perkel27@gmail.com>
2025-01-24 12:41:57 -0500

    5x5 grid

--- generate.py
+++ generate.py
@@ -6,8 +6,8 @@ start = 1
 sizes = [start * (0.9 ** i) for i in range(10)]
 
 for i in range(10):
-    for x in range(-2, 2):
-        for y in range(-2, 2):
+    for x in range(-2, 3):
+        for y in range(-2, 3):
             z = sizes[i] / 2
             pyrosim.Send_Cube(name=f"Box{i}", pos=[x,y,z+i] , size=[sizes[i],sizes[i],sizes[i]])
 
--- simulate.py
+++ simulate.py
@@ -12,7 +12,7 @@ planeId = p.loadURDF("plane.urdf")
 
 p.loadSDF("boxes.sdf")
 
-for i in range(1000):
+for i in range(2000):
     p.stepSimulation()
     time.sleep(1/60)
     print(i)
