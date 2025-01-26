90c0639492ed3df20b839cd6900756a2260c680a
aaronperkel <aaron.perkel27@gmail.com>
2025-01-26 16:50:01 -0500

    world in background, robot at origin

--- generate.py
+++ generate.py
@@ -10,10 +10,13 @@ def Create_World():
     Create an SDF file named 'world.sdf' containing a single cube.
     """
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5], size=[1, 1, 1])
+    pyrosim.Send_Cube(name="Box", pos=[-4, 4, 0.5], size=[1, 1, 1])
     pyrosim.End()
 
 def Create_Robot():
+    """
+    Create a URDF file named 'body.urdf' containing a single cube.
+    """
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0.5], size=[1, 1, 1])
     pyrosim.End()
