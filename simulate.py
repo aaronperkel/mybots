95681584fcc74f16e8f2b7ddf5b9323131ac5216
aaronperkel <aaron.perkel27@gmail.com>
2025-01-29 19:14:14 -0500

    joints complete

--- simulate.py
+++ simulate.py
@@ -7,24 +7,24 @@ sets gravity, and continuously steps through the simulation loop.
 import pybullet as p
 import pybullet_data
 import time
-import generate as g
 
-g.Create_World()
-g.Create_ThreeLinkTwoJoint()
+def main():
+    running = True
 
-running = True
+    physicsClient = p.connect(p.GUI)
+    p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+    p.setAdditionalSearchPath(pybullet_data.getDataPath())
+    p.setGravity(0,0,-9.8)
+    p.loadSDF("world.sdf")
 
-physicsClient = p.connect(p.GUI)
-p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
-p.setAdditionalSearchPath(pybullet_data.getDataPath())
-p.setGravity(0,0,-9.8)
-p.loadSDF("world.sdf")
+    planeId = p.loadURDF("plane.urdf")
+    robotID = p.loadURDF("three_link.urdf")
 
-planeId = p.loadURDF("plane.urdf")
-robotID = p.loadURDF("three_link.urdf")
+    while running:
+        p.stepSimulation()
+        time.sleep(.005)
 
-while running:
-    p.stepSimulation()
-    time.sleep(.005)
+    p.disconnect()
 
-p.disconnect()
+if __name__ == '__main__':
+    main()
