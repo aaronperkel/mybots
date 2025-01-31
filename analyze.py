a29592ce423ef843f1691aa457af2dc89717f801
aaronperkel <aaron.perkel27@gmail.com>
2025-01-31 11:40:06 -0500

    Prettifying the visualization

--- analyze.py
+++ analyze.py
@@ -10,11 +10,10 @@ def main():
     backLegSensorValues = np.load('data/backLegSensorValues.npy')
     frontLegSensorValues = np.load('data/frontLegSensorValues.npy')
 
-    plt.plot(backLegSensorValues, label="Back Leg")
+    plt.plot(backLegSensorValues, label="Back Leg", linewidth=3)
     plt.plot(frontLegSensorValues, label="Front Leg")
 
     plt.legend()
-
     plt.show()
 
 if __name__ == '__main__':
