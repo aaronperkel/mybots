59fa46e0b7ad3bb52887f255615b0003a0e0c984
aaronperkel <aaron.perkel27@gmail.com>
2025-02-19 12:41:43 -0500

    somehow i was wrong the whole time?

--- scripts/generate.py
+++ scripts/generate.py
@@ -86,7 +86,6 @@ def Generate_Body():
     Creates a 3-link robot with two revolute joints.
     """
     pyrosim.Start_URDF("data/body.urdf")
-    pyrosim.Start_URDF("data/body.urdf")
 
     pyrosim.Send_Cube(
         name="Torso",
