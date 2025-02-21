7f294a241ed6a079aa381b5f9d4414422de524e8
aaronperkel <aaron.perkel27@gmail.com>
2025-02-21 08:47:13 -0500

    housekeeping

--- scripts/constants.py
+++ scripts/constants.py
@@ -4,22 +4,13 @@ Holds global constants such as the number of simulation steps, motor frequencies
 and other physical constants (gravity, max force, etc.).
 """
 
-import numpy as np
-
-STEPS = 3500
-
+STEPS = 1000
 NUMBER_OF_GENERATIONS = 10
-
 POPULATION_SIZE = 10
-
-AMPLITUDE = np.pi/4
-FREQUENCYL = 10
-PHASE_OFFSET = 0
 
 GRAV_X = 0
 GRAV_Y = 0
 GRAV_Z = -9.8
 
 MAX_FORCE = 20
-
 SLEEP_TIME = 0.005
