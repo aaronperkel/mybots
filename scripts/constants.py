0523c09910ba0df7e2bfad0ded19350f60f43ede
aaronperkel <aaron.perkel27@gmail.com>
2025-03-17 11:07:18 -0400

    updated constants

--- scripts/constants.py
+++ scripts/constants.py
@@ -4,6 +4,8 @@ Holds global constants such as the number of simulation steps, motor frequencies
 and other physical constants (gravity, max force, etc.).
 """
 
+import numpy as np
+
 STEPS = 1000
 
 NUMBER_OF_GENERATIONS = 10
@@ -14,17 +16,10 @@ NUM_MOTOR_NEURONS = 8
 
 MOTOR_JOINT_RANGE = 0.4
 
-AMPLITUDE_BL = 1.0
-FREQUENCY_BL = 2.0
-PHASE_OFFSET_BL = 0
-
-AMPLITUDE_FL = 1.0
-FREQUENCY_FL = 2.0
-PHASE_OFFSET_FL = np.pi
-
 GRAV_X = 0
 GRAV_Y = 0
 GRAV_Z = -9.8
 
 MAX_FORCE = 20
+
 SLEEP_TIME = 0.005
